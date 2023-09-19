39f0324fe3667625aa44b96742d62bd920d848be
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-19 13:29:00 +0200

    CSS index et show

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ Actor.destroy_all
 Cast.destroy_all
 Category.destroy_all
 
-p 'Création des données'
+p 'Extraction des données'
 p '--------------------'
 url = "https://gist.githubusercontent.com/alexandremeunier/49533eebe2ec93b14d32b2333272f9f8/raw/924d89e2236ca6fa2ade7481c91bfbf858c49531/movies.json"
 movies_serialized = URI.open(url).read
@@ -27,24 +27,54 @@ movies = JSON.parse(movies_serialized)
 
 index = 0
 
-movies.take(100).each do |movie|
-  new_movie = Movie.create!(
-    title: movie['title'],
-    year: movie['year'],
-    rating: movie['rating'],
-    image: movie['image']
-  )
+p 'Création des films'
+p '--------------------'
+movies.take(20).each do |movie|
+
+  poster_url = movie['image']
+  uri = URI.parse(poster_url)
+  response = Net::HTTP.get_response(uri)
+
+  if response.is_a?(Net::HTTPSuccess)
+    new_movie = Movie.create!(
+      title: movie['title'],
+      year: movie['year'],
+      rating: movie['rating'],
+      image: movie['image']
+    )
+  else
+    new_movie = Movie.create!(
+      title: movie['title'],
+      year: movie['year'],
+      rating: movie['rating'],
+      image: "https://t3.ftcdn.net/jpg/03/34/83/22/360_F_334832255_IMxvzYRygjd20VlSaIAFZrQWjozQH6BQ.jpg"
+    )
+  end
+
   i = 0
+
+  p 'Création des acteurs'
+  p '--------------------'
   movie["actors"].each do |actor|
     if Actor.find_by(fullname: actor).nil?
+
       actor_avatar_url = movie['actor_facets'][i].split("|").first
-      Actor.create!(fullname: actor, avatar: actor_avatar_url)
+      uri = URI.parse(actor_avatar_url)
+      response = Net::HTTP.get_response(uri)
+
+      if response.is_a?(Net::HTTPSuccess)
+        Actor.create!(fullname: actor, avatar: actor_avatar_url)
+      else
+        Actor.create!(fullname: actor, avatar: "https://t3.ftcdn.net/jpg/03/34/83/22/360_F_334832255_IMxvzYRygjd20VlSaIAFZrQWjozQH6BQ.jpg")
+      end
       i += 1
     end
     Cast.create!(movie: new_movie, actor: Actor.find_by(fullname: actor))
   end
 
 
+  p 'Création des genres'
+  p '--------------------'
   movie["genre"].each do |genre|
     if Genre.find_by(content: genre).nil?
       Genre.create!(content: genre)
